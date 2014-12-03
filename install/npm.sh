f9d63b5b8363e943931804447aa0449df0cf054c
brownman <brownman556@gmail.com>
2014-12-03 13:07:10 +0200

    Create npm.sh

---
+++ install/npm.sh
@@ -0,0 +1,38 @@
+
+
+node1(){
+#Node
+RUN curl http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz | tar xz
+RUN mv node* node && \
+    ln -s /node/bin/node /usr/local/bin/node && \
+    ln -s /node/bin/npm /usr/local/bin/npm
+}
+
+npm1(){
+sudo npm update -g npm
+sudo npm install -g grunt-cli bower
+sudo npm install -g mean-cli@0.5
+}
+
+init1(){
+mean init myApp
+cd myApp; 
+sudo  npm install -g
+sudo  npm link
+}
+
+test1(){
+cd $HOME/myApp;
+grunt 
+}
+
+steps(){
+  myApp=$HOME/myApp
+  cd $HOME
+  
+  node1
+  npm1
+  init1
+  test1
+}
+steps
