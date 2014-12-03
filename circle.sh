a310b6faab409b99635a8220b9465bf10facdd99
brownman <brownman556@gmail.com>
2014-12-03 20:09:07 +0200

    try: fix tests

--- circle.sh
+++ circle.sh
@@ -2,5 +2,5 @@
 #/bin/bash -c 'RUN ps aux | grep mongo'
 #https://github.com/circleci?query=docker
 
-docker run -d -p 3000:3000 -e "SECRET_KEY_BASE=abcd1234" circleci/hello:$CIRCLE_SHA1; cd /opt/mean/myApp; grunt test; sleep 10
+docker run -d -p 3000:3000 -e "SECRET_KEY_BASE=abcd1234" brownman/runner2:$CIRCLE_SHA1  cd /opt/mean/myApp; grunt test; grunt; sleep 10
 curl --retry 10 --retry-delay 5 -v http://localhost:3000
