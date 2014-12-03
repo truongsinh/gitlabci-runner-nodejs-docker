c6fdacc10807c52e8f27a0846d711602accbbbfe
brownman <brownman556@gmail.com>
2014-12-03 13:17:36 +0200

    Update mean.sh

--- install/mean.sh
+++ install/mean.sh
@@ -2,8 +2,8 @@
 
 node1(){
 #Node
-RUN curl http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz | tar xz
-RUN mv node* node && \
+  curl http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz | tar xz
+  mv node* node && \
     ln -s /node/bin/node /usr/local/bin/node && \
     ln -s /node/bin/npm /usr/local/bin/npm
 }
