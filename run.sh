57f98d529c46089ecbe2b541d6dfa9c638d8f752
brownman <brownman556@gmail.com>
2014-12-03 11:17:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-#!/bin/bash/usr/sbin/sshd -D & LC_ALL=C.UTF-8 sudo -H -u mongodb mongod --nojournal & cd /home/mean/mean ; sleep 5s ;  sudo -u mean node server.js
+#!/bin/bash
+
+
+/usr/sbin/sshd -D &
+LC_ALL=C.UTF-8 sudo -H -u mongodb mongod --nojournal & 
+cd /home/mean/mean ; 
+sleep 5s ;  
+sudo -u mean node server.js
