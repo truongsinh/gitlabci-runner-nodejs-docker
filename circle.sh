a8c3bd1e22e8a96d1fc20491f817bda63c464d61
brownman <brownman556@gmail.com>
2014-12-04 11:31:17 +0200

    test1: inspect the container's ENV

--- circle.sh
+++ circle.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 #/bin/bash -c 'RUN ps aux | grep mongo'
 #https://github.com/circleci?query=docker
+# -p 3000:3000
 
-docker run -d -p 3000:3000 -e "SECRET_KEY_BASE=abcd1234" brownman/runner2 <<START
-cd /opt/mean1/myApp;
-ls -l;
-grunt test; 
-grunt;
-sleep 10
+
+docker run -d -e "SECRET_KEY_BASE=abcd1234" brownman/runner2 <<START
+env
+echo "SECRET_KEY_BASE: $SECRET_KEY_BASE"
 START
 
-curl --retry 10 --retry-delay 5 -v http://localhost:3000
+#cd /opt/mean1/myApp;
+#ls -l;
+#grunt test; 
+#grunt;
+#sleep 10
+#START
+
+#curl --retry 10 --retry-delay 5 -v http://localhost:3000
