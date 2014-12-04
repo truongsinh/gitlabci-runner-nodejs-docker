5b836b27907184de330d5d438f5c836c6c5b8719
brownman <brownman556@gmail.com>
2014-12-04 17:55:49 +0200

    fix locals
    use Ex: .sh

---
+++ install/fix_locals.sh
@@ -0,0 +1,13 @@
+
+ubuntu1304(){
+# Set the locale
+sudo locale-gen en_US.UTF-8  
+export LANG=en_US.UTF-8  
+export LANGUAGE=en_US:en  
+export LC_ALL=en_US.UTF-8  
+#RUN echo 'LC_ALL="en_US.UTF-8"' >> /etc/default/locale
+#RUN locale-gen en_US.UTF-8
+#RUN update-locale LANG=en_US.UTF-8
+}
+
+ubuntu1304
