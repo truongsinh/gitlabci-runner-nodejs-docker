c0268146ab1693209a935bf01808ab7b38e8c656
brownman <brownman556@gmail.com>
2014-12-04 13:22:40 +0200

    try: fix test

--- circle.sh
+++ circle.sh
@@ -3,11 +3,22 @@
 #https://github.com/circleci?query=docker
 # -p 3000:3000
 
+#
+ docker run brownman/runner3 sudo -u gitlab_ci_runner -H 'env' > /tmp/env.txt
+ cp /tmp/env.txt $CIRCLE_ARTIFACTS/env.txt
+ 
+  docker run brownman/runner3 sudo -u gitlab_ci_runner -H 'ps -aux' > /tmp/ps.txt
+ cp /tmp/ps.txt $CIRCLE_ARTIFACTS/ps.txt
+ 
+   docker run brownman/runner3 sudo -u gitlab_ci_runner -H 'netstat -ntlp' > /tmp/netstat.txt
+ cp /tmp/netstat.txt $CIRCLE_ARTIFACTS/netstat.txt
+ 
+ 
 
-docker run -d -e "SECRET_KEY_BASE=abcd1234" brownman/runner2 <<START
-env
-echo "SECRET_KEY_BASE: $SECRET_KEY_BASE"
-START
+#docker run -d -e "SECRET_KEY_BASE=abcd1234" brownman/runner2 <<START
+#env
+#echo "SECRET_KEY_BASE: $SECRET_KEY_BASE"
+#START
 
 #cd /opt/mean1/myApp;
 #ls -l;
