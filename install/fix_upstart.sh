8cf0ec3dfbd7e7ff63ddc5398b02b6a50875e880
brownman <brownman556@gmail.com>
2014-12-04 17:57:04 +0200

    Update fix_upstart.sh

--- install/fix_upstart.sh
+++ install/fix_upstart.sh
@@ -1,2 +1,3 @@
 # Fix upstart under a virtual host https://github.com/dotcloud/docker/issues/1024
 sudo  dpkg-divert --local --rename --add /sbin/initctl && rm -f /sbin/initctl && ln -s /bin/true /sbin/initctl
+#echo "#!/bin/sh\nexit 0" | sudo too /usr/sbin/policy-rc.d
