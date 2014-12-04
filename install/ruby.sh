b513a52aa2b57c99b911b532222085ba8e5d5981
brownman <brownman556@gmail.com>
2014-12-04 18:01:57 +0200

    cleaner: ruby instructions

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,17 +1,23 @@
-#https://gitlab.com/gitlab-org/gitlab-ci-runner/
+# https://gitlab.com/gitlab-org/gitlab-ci-runner/
+
+# Update your packages and install the ones that are needed to compile Ruby
+# Download Ruby and compile it
+
 
 ruby19(){
-# Update your packages and install the ones that are needed to compile Ruby
 sudo  apt-get update -y
 sudo  apt-get install -y wget curl gcc libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential zlib1g-dev openssh-server git-core libyaml-dev postfix libpq-dev libicu-dev
 
-# Download Ruby and compile it
-mkdir /tmp/ruby && cd /tmp/ruby && curl --progress http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz | tar xz
-cd /tmp/ruby/ruby-1.9.3-p392 && ./configure --disable-install-rdoc && make && sudo make install
+mkdir /tmp/ruby 
+cd /tmp/ruby 
+curl --progress http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz | tar xz
+cd /tmp/ruby/ruby-1.9.3-p392 
+./configure --disable-install-rdoc 
+make 
+sudo make install
 }
 
 ruby20(){
-# Download Ruby and compile it
 sudo apt-get update -y
 sudo apt-get install -y wget curl gcc libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential zlib1g-dev openssh-server git-core libyaml-dev postfix libpq-dev libicu-dev
 
@@ -22,6 +28,8 @@ cd ruby-2.0.0-p353
 make
 sudo make install
 }
+
+
 # don't install ruby rdocs or ri:
 install_bundler(){
 echo "gem: --no-rdoc --no-ri" | sudo tee /usr/local/etc/gemrc
@@ -29,4 +37,4 @@ sudo  gem install bundler
 }
 
 ruby20
-bundler
+install_bundler #+mute extra docs
