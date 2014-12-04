9b246739a43e8d1e0362af73106e3e2b5098c84b
brownman <brownman556@gmail.com>
2014-12-04 17:46:01 +0200

    nicer: install ruby and its deps

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,22 +1,17 @@
-# Download Ruby and compile it
+#https://gitlab.com/gitlab-org/gitlab-ci-runner/
 
-# Update your packages and install the ones that are needed to compile Ruby
-#RUN apt-get update -y
-#RUN apt-get upgrade -y
-#RUN apt-get install -y curl libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline6-dev libssl-dev patch build-essential zlib1g-dev openssh-server libyaml-dev libicu-dev
 
 # Download Ruby and compile it
-
 sudo apt-get update -y
 sudo apt-get install -y wget curl gcc libxml2-dev libxslt-dev libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential zlib1g-dev openssh-server git-core libyaml-dev postfix libpq-dev libicu-dev
-mkdir /tmp/ruby
-cd /tmp/ruby
-# curl --silent ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p481.tar.gz | tar xz
-curl -s http://ftp.ruby-lang.org/pub/ruby/ruby-2.0-stable.tar.bz2 | tar xj --strip-components=1
-cd /tmp/ruby/ruby-2.0.0-p481
-./configure --disable-install-rdoc 
+
+mkdir /tmp/ruby && cd /tmp/ruby
+curl --progress ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p353.tar.gz | tar xz
+cd ruby-2.0.0-p353
+./configure --disable-install-rdoc
+make
 sudo make install
-rm -rf /tmp/ruby
+
 
 # don't install ruby rdocs or ri:
 echo "gem: --no-rdoc --no-ri" >> /usr/local/etc/gemrc
