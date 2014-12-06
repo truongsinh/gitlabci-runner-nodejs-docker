6c48ba12a32589f4433bf72be3581f94d9fb3afe
brownman <brownman556@gmail.com>
2014-12-06 14:20:39 +0200

    3!

--- install/mongo.sh
+++ install/mongo.sh
@@ -1,6 +1,6 @@
 #MongoDB
 #https://www.digitalocean.com/community/tutorials/how-to-install-a-mean-js-stack-on-an-ubuntu-14-04-server
-DEBIAN_FRONTEND=noninteractive
+install(){
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
     echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list && \
@@ -9,3 +9,14 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
 sudo apt-get install -y mongodb-org
 sudo  mkdir -p /data/db
 sudo  chown -R mongodb:mongodb /data
+}
+
+config(){
+    sudo mongod &>/dev/null &
+}
+
+test(){
+  ensure  'sudo netstat -ntlp | grep mongo'
+}
+
+commander $@
