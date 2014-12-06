47cf05b93b37873764b37607d341edcfea000df3
brownman <brownman556@gmail.com>
2014-12-06 19:01:44 +0200

    docker: fix upstart

--- install/fix_upstart.sh
+++ install/fix_upstart.sh
@@ -1,5 +1,17 @@
+
+install(){
 trace Fix upstart under a virtual host https://github.com/dotcloud/docker/issues/1024
 sudo  dpkg-divert --local --rename --add /sbin/initctl 
 sudo rm -f /sbin/initctl 
 sudo ln -s /bin/true /sbin/initctl
+}
+
+config(){
+  trace
+}
+
+test(){
+  trace
+}
+commander $@
 #echo "#!/bin/sh\nexit 0" | sudo too /usr/sbin/policy-rc.d
