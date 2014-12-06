ece4d3521a8aba5ce1bc5681e1ee4e7ae52a4f84
brownman <brownman556@gmail.com>
2014-12-06 18:57:02 +0200

    mongo.sh: fix permission problem
    b.t.w: how did u make it till now ?

--- install/mongo.sh
+++ install/mongo.sh
@@ -1,11 +1,9 @@
 #MongoDB
 #https://www.digitalocean.com/community/tutorials/how-to-install-a-mean-js-stack-on-an-ubuntu-14-04-server
 install(){
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
-    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list && \
-    apt-get update
-    
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo  mkdir -p /data/db
 sudo  chown -R mongodb:mongodb /data
