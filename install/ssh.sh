f26bafd62a611df871ce929183852dbc2c6733d2
brownman <brownman556@gmail.com>
2014-12-06 14:24:54 +0200

    3!

--- install/ssh.sh
+++ install/ssh.sh
@@ -1,8 +1,20 @@
 #RUN mkdir /var/run/sshd
 #RUN echo 'root:mean22' | chpasswd
 #SSHD
+install(){
 sudo  apt-get install -y openssh-server
+
+}
+
+config(){
 sudo mkdir -p /var/run/sshd 
 echo 'root:root' | sudo tee chpasswd
 sudo sed -i "s/session.*required.*pam_loginuid.so/#session    required     pam_loginuid.so/" /etc/pam.d/sshd
 sudo sed -i "s/PermitRootLogin without-password/#PermitRootLogin without-password/" /etc/ssh/sshd_config
+}
+
+test(){
+  netstat -ntlp | grep sshd
+}
+
+commander $@
