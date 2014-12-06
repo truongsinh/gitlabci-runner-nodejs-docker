99cc1f47ba78ed300eb1184d08f07abe3bc9d29c
brownman <brownman556@gmail.com>
2014-12-06 18:41:48 +0200

    fix: ssh.sh

--- install/ssh.sh
+++ install/ssh.sh
@@ -7,14 +7,15 @@ sudo  apt-get install -y openssh-server
 }
 
 config(){
-sudo mkdir -p /var/run/sshd 
+  trace ''
+#sudo mkdir -p /var/run/sshd 
 #echo 'root:root' | sudo tee chpasswd
 
 #sudo su root <<START
 #START
 
-sudo sed -i "s/session.*required.*pam_loginuid.so/#session    required     pam_loginuid.so/" /etc/pam.d/sshd
-sudo sed -i "s/PermitRootLogin without-password/#PermitRootLogin without-password/" /etc/ssh/sshd_config
+#sudo sed -i "s/session.*required.*pam_loginuid.so/#session    required     pam_loginuid.so/" /etc/pam.d/sshd
+#sudo sed -i "s/PermitRootLogin without-password/#PermitRootLogin without-password/" /etc/ssh/sshd_config
 
 }
 
