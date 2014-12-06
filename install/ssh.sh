2591213f73b7e9fe8e78c7431bff05c12f1630e7
brownman <brownman556@gmail.com>
2014-12-06 18:05:48 +0200

    Update ssh.sh

--- install/ssh.sh
+++ install/ssh.sh
@@ -10,10 +10,12 @@ config(){
 sudo mkdir -p /var/run/sshd 
 #echo 'root:root' | sudo tee chpasswd
 
-sudo su root <<START
+#sudo su root <<START
+#START
+
 sudo sed -i "s/session.*required.*pam_loginuid.so/#session    required     pam_loginuid.so/" /etc/pam.d/sshd
 sudo sed -i "s/PermitRootLogin without-password/#PermitRootLogin without-password/" /etc/ssh/sshd_config
-START
+
 }
 
 test(){
