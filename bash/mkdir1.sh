c5f39a71a9bbaa24d9acd408baeda938e996fd42
brownman <brownman556@gmail.com>
2014-12-07 17:28:35 +0200

    test: bashism
    mkdir1

--- bash/mkdir1.sh
+++ bash/mkdir1.sh
@@ -1,36 +1,16 @@
-set_env(){
- # mkdir1 $dir_my_app
-  mkdir1 $dir_nodejs
-}
-node1(){
-  set_env
- # cd $dir
-  sudo apt-get install curl
-  cd /tmp
-  curl http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz | tar xz
-  mv /tmp/node* $dir_nodejs
-  sudo ln -s $dir_nodejs/bin/node /usr/local/bin/node
-  sudo ln -s $dir_nodejs/bin/npm /usr/local/bin/npm
-}
 
-install(){
- print_func
- echo node1
-}
-
-  
-config(){
- print_func
-  trace
-}
-test(){ 
+pass1(){ 
  print_func
 local  dir=/tmp/2
   mkdir1 $dir
   ensure "test -d $dir" 
 echo 1 > /tmp/1
 cat1 /tmp/1
-blabla
 }
 
-commander $@
+fail1(){
+ bla bla
+}
+
+pass1
+fail1
