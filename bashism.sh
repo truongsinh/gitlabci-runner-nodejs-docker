a445fc0cd7beef5988cbd8ef85f907f60ebbee82
brownman <brownman556@gmail.com>
2014-12-07 14:16:59 +0200

    test mkdir1()

--- bashism.sh
+++ bashism.sh
@@ -1,19 +1,11 @@
-export str_caller1='eval echo $(caller)'
-export str_caller2='echo $(caller)'
 
+middle=mkdir1
+source config.cfg
 
-
-func2(){
-$str_caller1
-$str_caller2
-eval "$str_caller1" 
-eval "$str_caller2" 
+step1(){
+local file="BASH/${middle}.sh"
+commander chmod +x $file
+commander $file
 }
 
-func1(){
-func2
-}
-
-
-
-func1
+step1
