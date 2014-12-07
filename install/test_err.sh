22f1084d595945b75fe25a27a63179d83b7752e8
brownman <brownman556@gmail.com>
2014-12-07 11:18:56 +0200

    test_err: inspect recent err

--- install/test_err.sh
+++ install/test_err.sh
@@ -1,6 +1,27 @@
-install(){
-  echo
+set_env(){
+ # mkdir1 $dir_my_app
+  mkdir1 $dir_nodejs
 }
+node1(){
+  set_env
+ # cd $dir
+  sudo apt-get install curl
+  cd /tmp
+  curl http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz | tar xz
+  mv /tmp/node* $dir_nodejs
+  sudo ln -s $dir_nodejs/bin/node /usr/local/bin/node
+  sudo ln -s $dir_nodejs/bin/npm /usr/local/bin/npm
+}
+
+install(){
+  node1
+}
+
+
+
+npm1(){
+  
+  
 config(){
   trace
 }
@@ -12,4 +33,5 @@ echo 1 > /tmp/1
 cat1 /tmp/1
 blabla
 }
+
 commander $@
