e521d0cbe3f7c207f7d8106b70dd32f7345b90da
brownman <brownman556@gmail.com>
2014-12-08 14:39:15 +0200

    ruby.sh: adding ppa: stdin err

--- install/ruby.sh
+++ install/ruby.sh
@@ -5,12 +5,13 @@
 set -u
 
 rsyslog1(){
-        export DEBIAN_FRONTEND=noninteractive
-
-        sed -i 's/# \(.*multiverse$\)/\1/g' /etc/apt/sources.list && \
-    apt-get update && apt-get -y -q install software-properties-common python-software-properties
-add-apt-repository ppa:adiscon/v8-stable
-apt-get update && apt-get -y -q install rsyslog
+export DEBIAN_FRONTEND=noninteractive
+sed -i 's/# \(.*multiverse$\)/\1/g' /etc/apt/sources.list && \
+apt-get -y update 
+apt-get -y -q install software-properties-common python-software-properties
+add-apt-repository -y ppa:adiscon/v8-stable
+apt-get -y update 
+apt-get -y -q install rsyslog
 
 sed 's/#$ModLoad imudp/$ModLoad imudp/' -i /etc/rsyslog.conf
 sed 's/#$UDPServerRun 514/$UDPServerRun 514/' -i /etc/rsyslog.conf
