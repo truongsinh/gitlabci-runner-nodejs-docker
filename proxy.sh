492d0aae2e7569174f8b2da1c493dcb10a118f36
brownman <brownman556@gmail.com>
2014-12-08 10:58:44 +0200

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,3 +2,14 @@
 #RUN ./fix/permission.sh
 #make travis and docker passing step after step together
 #RUN ./travis.sh
+echo "---------------------"
+echo "dir_start:     $PWD"
+echo "---------------------"
+
+
+
+source config.cfg
+./fix/permission.sh
+whoami
+
+ls travis.sh
