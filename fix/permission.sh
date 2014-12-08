d95a56789411ba3495494779005ae86cef6a8497
brownman <brownman556@gmail.com>
2014-12-08 13:15:17 +0200

    permission: add a guest user
    [skip ci]

--- fix/permission.sh
+++ fix/permission.sh
@@ -1,6 +1,13 @@
 
+user_guest(){
+  sudo adduser guest --disabled-password --gecos "" 
+}
+iser_docker(){
 sudo useradd docker
 echo "docker:docker" | chpasswd
 sudo adduser docker sudo
 sudo mkdir -p /home/docker
 sudo chown -R docker:docker /home/docker
+}
+
+docker_user
